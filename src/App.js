d27659ca0683430335e53478e8b3c120a71010bd
functionup-am <akshaymakwana350@gmail.com>
2023-10-01 12:35:03 +0530

    url-frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import {useCopyToClipboard} from "usehooks-ts"
+import Layout from "./layout";
+const UrlHandler = () => {
+  const [longUrl, setLongUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+  const [value, copy] = useCopyToClipboard();
+  const [globalError,setGlobalError] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
+  const postData = async(data) => {
+   const Data = await fetch("http://localhost:8000/url/shorten", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        longUrl: data,
+      })
+    })
+    const storedData =await Data.json();
+    if(storedData.status===false){
+      setGlobalError(storedData.message)
+    }else{
+   
+    setShortUrl(storedData.data.shortUrl);
+  }
+    setLongUrl("");
+    setIsCopied(false);
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Layout/>
+    <div className="background-image-container">     
+           
+      <div className="input">        
+          <label className="heading" htmlFor="longurl">Short Your LongUrl : </label><br/>
+          <input className="box" type="text" size={50} placeholder="Paste here longUrl"  value={longUrl}  onChange={(e) => setLongUrl(e.target.value)}/><br/>
+          <div className="errorMessage">{globalError && <span>{globalError}</span>}</div>
+          <button type="submit" className="btn btn-outline-primary" onClick={()=> postData(longUrl)}>Convert</button><br />
+         
+          <h5 className="output">{shortUrl}</h5> <button  className="btn btn-outline-primary" onClick={() => {
+      if (shortUrl) {
+        copy(shortUrl);
+        setIsCopied(true);
+      }
+    }}> {isCopied ? "Copied" : "Copy"}</button>
+
+      </div>
+    </div>
     </div>
   );
-}
-
-export default App;
+};
+export default UrlHandler;
---
+++ src/layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css";
+const Layout = () => {
+  return (
+    <div className="body">
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#services">Services</a>
+    <a href="#about">About Us</a>
+  </nav>
+    </div>
+  )
+}
+
+export default Layout;
